bdae98158eca689371fe52b88b438c6bebca4144
Hendra Gunawan <48410066+JadlionHD@users.noreply.github.com>
2024-10-11 23:25:38 +0700

    feat: update router

--- main.go
+++ main.go
@@ -1,31 +1,17 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-
 	"github.com/JadlionHD/crud-gin-go/controllers"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
-	router.GET("/posts/:id", func(c *gin.Context) {
-		id := c.Param("id")
-		data, err := controllers.GetPostDummies(id)
 
-		if err != nil {
-			c.JSON(http.StatusNotFound, gin.H{
-				"message": fmt.Sprintf("Posts %s not found", id),
-			})
-			return
-		}
+	router.GET("/posts/:id", controllers.GetPost)
+	router.POST("/posts", controllers.CreatePost)
+	router.DELETE("/posts/:id", controllers.DeletePost)
+	router.PUT("/posts/:id", controllers.UpdatePost)
 
-		c.JSON(http.StatusOK, data)
-	})
-
-	router.POST("/posts/:id", func(ctx *gin.Context) {
-
-	})
 	router.Run("localhost:8080")
 }
